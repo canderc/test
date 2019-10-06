75d2d146b1fafa170f6d2e4e8828db7f6eb779b8
canderc <49619380+canderc@users.noreply.github.com>
2019-10-06 23:13:08 +0300

    add footer/faq

---
+++ js/naming.js
@@ -0,0 +1,14 @@
+var faq = document.getElementsByClassName("faq_title");
+
+for (var i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+          panel.style.display = "none";
+      } else {
+          panel.style.display = "block";
+          panel.style.borderBottom = "1px solid black"
+      }
+  });
+}
